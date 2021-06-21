9694a5a22fa5c3f1d614d51043848bc8e7b6e0a1
yutaka918 <83869387+yutaka918@users.noreply.github.com>
2021-06-21 16:06:53 +0900

    Add files via upload

--- staff_login_check.php
+++ staff_login_check.php
@@ -1,10 +1,48 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>ろくまる農園</title>
-</head>
-<body>
+<?php
 
-</body>
-</html>
+try
+{
+
+    $staff_code=$_POST['code'];
+    $staff_pass=$_POST['pass'];
+
+    $staff_code=htmlspecialchars($staff_code,ENT_QUOTES,'UTF-8');
+    $staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+    $staff_pass=md5($staff_pass);
+    //mst_productにmst_staff入れる
+    $dsn = 'mysql:dbname=mst_product;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT name FROM mst_staff WHERE code=? AND password=?';
+    $stmt = $dbh->prepare($sql);
+    $data[]=$staff_code;
+    $data[]=$staff_pass;
+    $stmt->execute($data);
+
+    $dbh = null;
+
+    $rec = $stmt->fetch(PDO::FETCH_AASSOC);
+
+    if($rec==false)
+    {
+        print'スタッフコードがパスワードが間違っています。<br />';
+        print'<a href="staff_login.html">戻る</a>';
+    }
+    else
+    {
+        header('Location:staff_top.php');
+        exit();
+    }
+
+}
+catch(Exception $e)
+{
+    print 'ただいま障害により大変ご迷惑をお掛けしております。';
+    exit();
+}
+
+?>
